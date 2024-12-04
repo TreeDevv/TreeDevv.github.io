67e49c210ea4377cf3287caea76f259ccdd2453d
TreeDevv <hpaulbus@gmail.com>
2024-12-04 06:22:53 -0800

    initial

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<main>
+				<h1>
+					Hello, My name is Tree <br /> I'm a Professional Programmer
+				</h1>
+
+				<section className="work">
+					<h2>My Work</h2>
+
+					<div className="project">
+						<h3>Rogue Reborn</h3>
+						<p>
+							<i>11/2/24 - 11/30/24</i>
+						</p>
+					</div>
+					<div className="project">
+						<h3>Fisch</h3>
+						<p>
+							<i>11/2/24 - 11/30/24</i>
+						</p>
+					</div>
+					<div className="project">
+						<h3>Anime Fighters</h3>
+						<p>
+							<i>11/2/24 - 11/30/24</i>
+						</p>
+					</div>
+					<div className="project">
+						<h3>Project Slayers</h3>
+						<p>
+							<i>11/2/24 - 11/30/24</i>
+						</p>
+					</div>
+				</section>
+				<section className="contact">
+					<h2>Contact Me</h2>
+				</section>
+			</main>
+		</>
+	);
+}
+
+export default App;
